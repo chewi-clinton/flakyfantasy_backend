c7097a80c8e21570815d57301dcaf21068c2bd06
chewi-clinton <suhchenwi48@gmail.com>
2025-10-19 13:18:39 +0100

    dsxfgsdg

--- flaky_fantasy_backend/flaky_fantasy_backend/settings.py
+++ flaky_fantasy_backend/flaky_fantasy_backend/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
@@ -9,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'django-insecure-am5(5md1os1#x8m-7je5xzyy)exjl8#^hdcik_f4+!$%@)w#9k'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.3.1','0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.3.1', '0.0.0.0']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,8 +36,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173', 
-    
+    'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://192.168.3.1:5173',
 ]
@@ -104,6 +102,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
