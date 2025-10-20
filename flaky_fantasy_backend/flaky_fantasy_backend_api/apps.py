45c8206fac400c61f8a34c2daebcb41af26c93d4
chewi-clinton <suhchenwi48@gmail.com>
2025-10-20 16:17:30 +0100

    hh

--- flaky_fantasy_backend/flaky_fantasy_backend_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlakyFantasyBackendApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'flaky_fantasy_backend_api'
