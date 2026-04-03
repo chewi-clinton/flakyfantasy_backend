03e2e6dfc0b4ce387884778e5a33bb0210d9073d
chewi-clinton <suhchenwi48@gmail.com>
2026-04-03 07:33:03 +0100

    Add static file serving in debug mode

--- flaky_fantasy_backend/flaky_fantasy_backend/urls.py
+++ flaky_fantasy_backend/flaky_fantasy_backend/urls.py
@@ -6,7 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('flaky_fantasy_backend_api.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
